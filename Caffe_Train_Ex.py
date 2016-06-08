4da5ea2280e08ad675864f5d60788e24e261207f
nlunscher <nolan_lunscher@hotmail.com>
2016-06-07 22:13:15 -0400

    Train Example up the end

--- Caffe_Train_Ex.py
+++ Caffe_Train_Ex.py
@@ -4,6 +4,7 @@
 # Setup environment
 from pylab import *
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 #### Caffe Setup
@@ -81,27 +82,86 @@ print 'Train labels: ', solver.net.blobs['label'].data[:8]
 plt.imshow(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1,0,2).reshape(28,8*28),
 		cmap = 'gray'); axis('off')
 print 'Test labels: ', solver.test_nets[0].blobs['label'].data[:8]
+print
 # plt.show()
 
 
 #### Stepping the solver
 # show filters before training
-plt.subplot(2,1,1)
-plt.imshow(solver.net.params['conv1'][0].diff[:,0].reshape(4,5,5,5).transpose(0,2,1,3).reshape(4*5,5*5),
-		cmap = 'gray'); axis('off')
-# take 1 step of minibatch SGD
-solver.step(1)
-# check our filters after 1 step- first layer 4x5 grid of 5x5 filters
-plt.subplot(2,1,2)
-plt.imshow(solver.net.params['conv1'][0].diff[:,0].reshape(4,5,5,5).transpose(0,2,1,3).reshape(4*5,5*5),
-		cmap = 'gray'); axis('off')
-plt.show()
+if False:
+	plt.subplot(2,1,1)
+	plt.imshow(solver.net.params['conv1'][0].diff[:,0].reshape(4,5,5,5).transpose(0,2,1,3).reshape(4*5,5*5),
+			cmap = 'gray'); axis('off')
+	#take 1 step of minibatch SGD
+	solver.step(1)
+	# check our filters after 1 step- first layer 4x5 grid of 5x5 filters
+	plt.subplot(2,1,2)
+	plt.imshow(solver.net.params['conv1'][0].diff[:,0].reshape(4,5,5,5).transpose(0,2,1,3).reshape(4*5,5*5),
+			cmap = 'gray'); axis('off')
+print
+# plt.show()
 
 
 #### Custom Training Loop
+# cahnge the solving process by updating the net in a loop
+niter = 50 #200
+test_interval = 25
+#losses will also be stored in the log
+train_loss = zeros(niter)
+test_acc = zeros(int(np.ceil(niter / test_interval)))
+output = zeros((niter, 8, 10))
 
+if True:
+	print "Start Training Loop"
+	# the main solver loop
+	for it in range(niter):
+		# print "Iteration ", it
+		solver.step(1) 	# SGD by caffe
 
+		#store the train loss
+		train_loss[it] = solver.net.blobs['loss'].data
 
+		# store the output on the first test batch
+		# (start the forward pass at conv1 to avoid loading new data)
+		solver.test_nets[0].forward(start = 'conv1')
+		output[it] = solver.test_nets[0].blobs['score'].data[:8]
+
+		# run a full test every so often
+		# (caffe can also do this for us and write to a log, but we show here
+		# how to do it directly in python, where more complicated things are easier)
+		if it % test_interval == 0:
+			print "Iteration ", it, " testing..."
+			correct = 0
+			for test_it in range(100):
+				solver.test_nets[0].forward()
+				correct += sum(solver.test_nets[0].blobs['score'].data.argmax(1) ==
+								solver.test_nets[0].blobs['label'].data)
+			test_acc[it // test_interval] = correct
+
+	#plot the train loss and test accuracy
+	_, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+	ax1.plot(np.arange(niter), train_loss)
+	ax2.plot(test_interval * np.arange(len(test_acc)), test_acc, 'r')
+	ax1.set_xlabel('iteration')
+	ax1.set_ylabel('train loss')
+	ax2.set_ylabel('test accuracy')
+	ax2.set_title('Test Accuracy: {:.2f}'.format(test_acc[-1]))
+	plt.show()
+
+	# show the prediction scores evolve over time
+	# for i in range(8):
+	# 	plt.subplot(2,1,1)
+	# 	plt.figure(figsize = (2,2))
+	# 	plt.imshow(solver.test_nets[0].blobs['data'].data[i,0], cmap='gray')
+	# 	plt.subplot(2,1,2)
+	# 	plt.figure(figsize=(10,2))
+	# 	plt.imshow(output[:50, i].T, interpolation = 'nearest', cmap='gray')
+	# 	plt.xlabel('iteration')
+	# 	plt.ylabel('label')
+	# plt.show()
+
+print
 
 
 
